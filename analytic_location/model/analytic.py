85714079df627cec07f873ce3970031b209e1ccf
aheficent <ahenriquez@eficent.com>
2017-07-03 15:18:21 +0200

    [UPT]analytic_location code to new api

--- analytic_location/model/analytic.py
+++ analytic_location/model/analytic.py
@@ -3,63 +3,45 @@
 # © 2016 Matmoz d.o.o.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp.osv import fields, orm
+from openerp import api, fields, models
 
 
-class AccountAnalyticAccount(orm.Model):
+class AccountAnalyticAccount(models.Model):
     _inherit = 'account.analytic.account'
 
-    _columns = {
-        'warehouse_id': fields.many2one(
-            'stock.warehouse',
-            'Warehouse',
-        ),
-        'location_id': fields.many2one(
-            'stock.location',
-            'Default Stock Location',
-            domain=[('usage', '<>', 'view')]
-        ),
-        'dest_address_id': fields.many2one(
-            'res.partner',
-            'Delivery Address',
-            help="Delivery address for "
-            "the current contract "
-            "/project."
-        ),
-    }
-
-    def _default_warehouse(self, cr, uid, context=None):
-        warehouse_obj = self.pool.get('stock.warehouse')
-        company_obj = self.pool.get('res.company')
-        company_id = company_obj._company_default_get(
-            cr, uid,
-            'stock.warehouse',
-            context=context
-        )
-        if context is None:
-            context = {}
-
+    @api.model
+    def _default_warehouse(self):
+        warehouse_obj = self.env['stock.warehouse']
+        company_obj = self.env['res.company']
+        company_id = company_obj._company_default_get('stock.warehouse')
         warehouse_ids = warehouse_obj.search(
-            cr, uid, [('company_id', '=', company_id)], limit=1,
-            context=context) or []
-
+            [('company_id', '=', company_id)], limit=1) or []
         if warehouse_ids:
             return warehouse_ids[0]
         else:
             return False
 
-    def _default_dest_address(self, cr, uid, context=None):
-        if context is None:
-            context = {}
-        partner_id = context.get('partner_id', False)
+    @api.model
+    def _default_dest_address(self):
+        partner_id = self.env.context.get('partner_id', False)
         if partner_id:
-            return self.pool.get('res.partner').address_get(
-                cr, uid, [partner_id], ['delivery']
+            return self.env['res.partner'].address_get(
+                [partner_id], ['delivery']
             )['delivery'],
         else:
             return False
 
-    _defaults = {
-        'warehouse_id': _default_warehouse,
-        'dest_address_id': _default_dest_address,
-    }
+    warehouse_id = fields.Many2one(
+        'stock.warehouse',
+        'Warehouse', default=_default_warehouse)
+    location_id = fields.Many2one(
+        'stock.location',
+        'Default Stock Location',
+        domain=[('usage', '<>', 'view')])
+    dest_address_id = fields.Many2one(
+        'res.partner',
+        'Delivery Address',
+        default=_default_dest_address,
+        help="Delivery address for "
+        "the current contract "
+        "/project.")
